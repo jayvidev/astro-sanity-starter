d854f1c201db48eee49658ac50aaade8ad9c7d37
jayvidev <jason.vilac@gmail.com>
2026-06-13 02:38:19 -0500

    refactor: simplify organization schema to base properties and remove optional contact fields

--- src/lib/structured-data.ts
+++ src/lib/structured-data.ts
@@ -8,42 +8,15 @@ import type { SettingsView } from '@/lib/content'
 
 export function buildOrganizationSchema(settings: SettingsView) {
   const { url } = config.site
-  const phone = settings.companyPhone?.replace(/[^\d+]/g, '')
-  const addr = settings.structuredAddress
-  const geo = settings.geo
-  const hours = settings.openingHours
-  const sameAs = settings.socialLinks.map((s) => s.url).filter(Boolean)
 
   return {
     '@context': 'https://schema.org',
-    '@type': settings.businessType || 'Organization',
+    '@type': 'Organization',
     name: settings.companyName,
     ...(settings.defaultSeo?.ogImage && {
       image: new URL(settings.defaultSeo.ogImage, url).href,
     }),
     '@id': `${url}/#organization`,
     url,
-    ...(phone && { telephone: phone }),
-    ...(settings.companyEmail && { email: settings.companyEmail }),
-    ...(addr?.streetAddress && {
-      address: { '@type': 'PostalAddress', ...addr },
-    }),
-    ...(geo?.latitude != null &&
-      geo?.longitude != null && {
-        geo: {
-          '@type': 'GeoCoordinates',
-          latitude: geo.latitude,
-          longitude: geo.longitude,
-        },
-      }),
-    ...(hours?.days?.length && {
-      openingHoursSpecification: {
-        '@type': 'OpeningHoursSpecification',
-        dayOfWeek: hours.days,
-        ...(hours.opens && { opens: hours.opens }),
-        ...(hours.closes && { closes: hours.closes }),
-      },
-    }),
-    ...(sameAs.length && { sameAs }),
   }
 }
